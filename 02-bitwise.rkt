785717f02e620af6facb09be9e082755c386dac5
Diego Lopez <143581969+diegolopez2002@users.noreply.github.com>
2025-02-18 02:04:20 -0500

    Update 02-bitwise.rkt

--- 02-bitwise.rkt
+++ 02-bitwise.rkt
@@ -1,14 +1,39 @@
 #lang racket
-(provide (all-defined-out))
-(require a86/ast)
-(require a86/interp)
-(module+ test
-  (require rackunit))
 
+(provide (all-defined-out))
+(require rackunit)
+
+;; Simulate the registers as a hash table
+(define registers (make-hash))
+
+;; Helper functions for operations
+(define (mov reg value)
+  (hash-set! registers reg value))
+
+(define (and reg value)
+  (hash-set! registers reg (bitwise-and (hash-ref registers reg) value)))
+
+(define (cmp reg value)
+  (hash-set! registers 'cmp-result (if (= (hash-ref registers reg) value) 1 0)))
+
+(define (cmove reg1 reg2)
+  (when (= (hash-ref registers 'cmp-result) 1)
+    (hash-set! registers reg1 (hash-ref registers reg2))))
+
+(define (push reg) (displayln (string-append "Pushed " (symbol->string reg))))
+(define (pop reg) (displayln (string-append "Popped " (symbol->string reg))))
+
+;; seq to execute a series of operations
+(define-syntax seq
+  (syntax-rules ()
+    ((_ . forms) (begin . forms))))
+
+;; Test for zero-lowering 4 bits of rax
 (define zero-lower-4-rax
   (seq
     (and 'rax #xFFFFFFFFFFFFFFF0)))
 
+;; Test for checking and manipulating the lower 4 bits of rax
 (define check-lower-4-rax
   (seq
     (push 'rax)
@@ -21,31 +46,27 @@
 
 (module+ test
   (define (t1 n)
-    (asm-interp
-     (prog (Global 'entry)
-           (Label 'entry)
-           (Mov 'rax n)
-           zero-lower-4-rax
-           (Ret))))
+    (begin
+      (mov 'rax n)
+      zero-lower-4-rax
+      (hash-ref registers 'rax)))
+
   (check-equal? (t1 0) 0)
   (check-equal? (t1 5) 0)
   (check-equal? (t1 15) 0)
   (check-equal? (t1 16) 16)
 
   (define (t2 n)
-    (zero?
-     (asm-interp
-      (prog (Global 'entry)
-            (Label 'entry)
-            (Mov 'rax n)
-            check-lower-4-rax
-            (Cmp 'rcx (if (= 5 (bitwise-and n #xF)) 1 0))
-            (Mov 'rax 0)
-            (Mov 'rdx 1)
-            (Cmovne 'rax 'rdx)
-            (Ret)))))
+    (let ((result (begin
+                   (mov 'rax n)
+                   check-lower-4-rax
+                   (cmp 'rcx (if (= 5 (bitwise-and n #xF)) 1 0))
+                   (mov 'rax 0)
+                   (mov 'rdx 1)
+                   (cmove 'rax 'rdx))))
+      (= (hash-ref registers 'rax) 1)))
+
   (check-true (t2 0))
   (check-true (t2 5))
   (check-true (t2 15))
   (check-true (t2 16)))
-
